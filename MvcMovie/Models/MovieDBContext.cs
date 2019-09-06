e23a3b969e5e3d484ddbe3d54706fdd1aff27258
SamirJan <samir-1966@mail.ru>
2019-09-06 13:15:50 +0500

    1 commit

---
+++ MvcMovie/Models/MovieDBContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
